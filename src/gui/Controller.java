dbaf2d3247ef27bff08dac351dcb743761414dd1
Your Name <you@example.com>
2021-09-09 12:17:21 -0300

    adicioando scene

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,17 +1,31 @@
 package gui;
 
+import java.awt.Event;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.scene.input.*;
-
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 
 public class Controller implements Initializable{
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 	
 	@FXML
 	private TextField cepInput;
@@ -23,18 +37,31 @@ public class Controller implements Initializable{
 	
 	private String cep;
 	
+	public void switchScene(ActionEvent event, String src) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(src));
+		// source scene by button
+		Node source = (Node) event.getSource();
+		stage = (Stage) (source).getScene().getWindow();
+		root = loader.load();
+		scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+	}
+	
 	@FXML
 	public void formHandle(KeyEvent event) {
 		
 		// se pressionado enter
 		if(event.getCode().equals(KeyCode.ENTER)) {
-			this.submit();
+			System.out.println("OK");
 		}
 		
 	}
 	
 	@FXML
-	public void submit() {
+	public void submit(ActionEvent event) throws IOException {
 
 		cep = cepInput.getText();
 		
@@ -42,6 +69,9 @@ public class Controller implements Initializable{
 			String searchMsg = String.format("Busca por: %s", cep);
 	
 			searched.setText(searchMsg);
+			
+			this.switchScene(event, "/gui/Result.fxml");
+			
 		}else {
 			searched.setText("");
 		}
@@ -52,8 +82,7 @@ public class Controller implements Initializable{
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		
-		
+	
 	}
 	
 
---
+++ src/gui/ResultController.java
@@ -0,0 +1,11 @@
+package gui;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Controller;
+import javafx.fxml.Initializable;
+
+public class ResultController {
+	
+	
+}
